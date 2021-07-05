fec0c8e126be21b51c243fd7ea1d6d2bc7c12953
HaiNguyen2903 <hainguyen29031412@gmail.com>
2021-07-05 19:14:24 +0700

    change path

--- configs/my_config/mask_rcnn_person.py
+++ configs/my_config/mask_rcnn_person.py
@@ -16,13 +16,23 @@ data = dict(
         classes=classes,
         ann_file='/Users/hainguyen/Documents/deep_learning_projects/open-mmlab/data/annotations_coco_format/instances_default.json'),
     val=dict(
-        img_prefix='/Users/hainguyen/Documents/deep_learning_projects/open-mmlab/data/frame_data_2',
+        # unnote if running local
+        # img_prefix='/Users/hainguyen/Documents/deep_learning_projects/open-mmlab/data/frame_data_2',
+
+        # unnote if run on colab
+        img_prefix = '/content/drive/Shareddrives/Giang/HaiNguyen/UET_AI_Lab/frame_data_2',
         classes=classes,
-        ann_file='data'),
+        
+        # unnote if run on colab
+        ann_file='/content/drive/Shareddrives/Giang/HaiNguyen/UET_AI_Lab/annotations_coco_format/instances_default.json'),
     test=dict(
-        img_prefix='/Users/hainguyen/Documents/deep_learning_projects/open-mmlab/data/frame_data_2',
+        # img_prefix='/Users/hainguyen/Documents/deep_learning_projects/open-mmlab/data/frame_data_2',
+        # unnote if run on colab
+        img_prefix = '/content/drive/Shareddrives/Giang/HaiNguyen/UET_AI_Lab/frame_data_2',
         classes=classes,
-        ann_file='/Users/hainguyen/Documents/deep_learning_projects/open-mmlab/data/annotations_coco_format/instances_default.json'))
+        # unnote if running local
+        # ann_file='/Users/hainguyen/Documents/deep_learning_projects/open-mmlab/data/annotations_coco_format/instances_default.json'))
+        ann_file='/content/drive/Shareddrives/Giang/HaiNguyen/UET_AI_Lab/annotations_coco_format/instances_default.json'))
 
 # We can use the pre-trained Mask RCNN model to obtain higher performance
 load_from = 'checkpoints/mask_rcnn_r50_caffe_fpn_mstrain-poly_3x_coco_bbox_mAP-0.408__segm_mAP-0.37_20200504_163245-42aa3d00.pth'
